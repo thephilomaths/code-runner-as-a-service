9fc0d873b9b125e0ec465da28c1bb3f96eeb0097
Kartik <kartikbehl99@gmail.com>
2021-02-01 16:04:05 +0530

    Fix(Docker) - fix quotes

--- init_rabbitmq.sh
+++ init_rabbitmq.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 # run this scrip after starting all the containers
 
-docker-compose exec rabbitmqctl add_user code_runner yeeyeeasshaircut &&
-docker-compose exec rabbitmqctl set_user_tags code_runner administrator &&
-docker-compose exec rabbitmqctl set_permissions -p / code_runner ".*" ".*" ".*""
+docker-compose exec rabbit-mq rabbitmqctl add_user code_runner yeeyeeasshaircut
+docker-compose exec rabbit-mq rabbitmqctl set_user_tags code_runner administrator
+docker-compose exec rabbit-mq rabbitmqctl set_permissions -p / code_runner ".*" ".*" ".*"
